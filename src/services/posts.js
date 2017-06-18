d434c8f09e678409659ff0610f640d7ff31cb290
Robert Anderson <robert@noisysocks.com>
2017-06-18 17:17:34 -0400

    don't need lodash

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,18 +1,16 @@
-import _, { range, filter } from 'lodash';
 import moment from 'moment';
 import sampleData from '../sampleData';
 
 export function getProviders() {
-  return _(sampleData.posts).map('provider').uniq().value();
+  return [...new Set(sampleData.posts.map(post => post.provider))].sort();
 }
 
 export function getDays(offset = 0, limit = 15) {
-  return range(limit).map(n => moment().add(offset + n, 'days'));
+  return [...Array(limit).keys()].map(n => moment().add(offset + n, 'days'));
 }
 
 export function findPosts(provider, day) {
-  return filter(
-    sampleData.posts,
+  return sampleData.posts.filter(
     post => post.provider === provider && moment(post.date).isSame(day, 'day')
   );
 }
