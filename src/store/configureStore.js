636586fd81d268ec53b51f20976fa7234941626f
Robert Anderson <robert@noisysocks.com>
2017-06-25 19:31:22 -0400

    clean up configureStore slightly

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,10 +3,9 @@ import persistState from 'redux-localstorage';
 import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import posts from './ducks/posts';
 
-const reducer = combineReducers({
-  posts,
-});
+const reducer = combineReducers({ posts });
+const enhancer = compose(applyMiddleware(logger), persistState());
 
 export default function configureStore() {
-  return createStore(reducer, compose(applyMiddleware(logger), persistState()));
+  return createStore(reducer, enhancer);
 }
